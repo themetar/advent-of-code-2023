1946e8883430c1d78c02d85b8bce6d965b6b2b06
Dimitar Apostolovski <d.apostolovski@protonmail.com>
2023-12-06 20:13:43 +0100

    Replace O(n) algorithm with O(1) algorithm for 06

--- solutions/06.rb
+++ solutions/06.rb
@@ -16,6 +16,33 @@ module Day06
   end
 
   def self.ways_to_beat(time, distance)
-    1.upto(time / 2).count { |push_time| (time - push_time) * push_time > distance } * 2 - (time.even? && (time / 2) ** 2 > distance ? 1 : 0)
+    # uses quadratic equation
+
+    # x + y = time; x * y > distance =>
+    # => y = time - x; x * (time - x) > distance
+    # => -x^2 + time*x - distance > 0
+    
+    # a = -1, b = time, c = - distance
+    #
+    # zero solutions are (-b +- sqrt(b^2 - 4ac))/2a
+    # i.e. -b/2a +- sqrt(b^2 - 4ac)/2a
+
+    mid = time / 2.0
+
+    under_sqrt = time ** 2 - 4 * distance
+    
+    return 0 if under_sqrt < 0
+
+    part = Math.sqrt(under_sqrt) / -2
+
+    left = mid + part
+    right = mid - part
+
+    # we're looking for integer solutions that give bigger than 0 result
+    left = left == left.ceil && left + 1 || left.ceil
+    right = right == right.floor && right - 1 || right.floor
+
+    # return number of integers between left and right
+    right - left + 1
   end
 end
